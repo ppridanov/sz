faf3cc16a1b4dcfda6583536fcacdb5d20b53899
Petr Pridanov <p.pridanoff2016@yandex.kz>
2020-11-01 08:43:50 +0600

    Refactor mobile menu

--- js/script.js
+++ js/script.js
@@ -7,25 +7,28 @@
 //         allowPageScroll:"vertical"
 //     });
 // });
-const navBar = document.querySelector('.navbar-nav'),
-    mobileMenu = document.querySelector('.mobile-menu');
+const navLangButton = document.querySelector('.nav-lang');
+
+//     mobileMenu = document.querySelector('.mobile-menu');
 
 
-function hideNavEl() {
-    navBar.classList.add('hide-element');
-    mobileMenu.classList.remove('hide-element');
-}
-function showNavEl() {
-    navBar.classList.remove('hide-element');
-    mobileMenu.classList.add('hide-element');
-}
+// function hideNavEl() {
+//     navBar.classList.add('hide-element');
+//     mobileMenu.classList.remove('hide-element');
+// }
+// function showNavEl() {
+//     navBar.classList.remove('hide-element');
+//     mobileMenu.classList.add('hide-element');
+// }
 
 $(window).on('load resize', function () {
-    if ($(this).width() <= 1000) {
-        hideNavEl();
+    if ($(this).width() <= 991) {
+        $('.nav-lang').attr('class', 'nav-item nav-lang');
+        $('.btn-lang').attr('class', 'nav-link btn-lang');
     } 
-    if ($(this).width() >= 1001) {
-        showNavEl();
+    if ($(this).width() >= 992) {
+        $('.nav-lang').attr('class', 'd-flex pl-3 nav-lang pr-3 nav-item border-sides align-items-center');
+        $('.btn-lang').attr('class', 'btn btn-lang btn-outline-info btn-sm');
     }
 })
 $(document).ready(function () {
@@ -39,10 +42,12 @@ $(document).ready(function () {
         loop: true,
         margin: 8,
         dots: false,
+        autoWidth: true,
         responsive:{
             0:{
                 items:2,
                 margin: 8,
+                loop: false
             },
             500: {
                 items:3,
@@ -51,13 +56,13 @@ $(document).ready(function () {
             },
             1000: {
                 items:3,
-                loop: false,
+                loop: true,
                 margin: 20,
             },
             1200: {
                 items:4,
-                loop: false,
-                margin: 24,
+                loop: true,
+                margin: 21,
             }
         }
     });
